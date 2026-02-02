8cb2a6af6a22386d6224e4ef37c7116ee0b25edb
Merkulova Elizaveta <kurlyk013@gmail.com>
2026-02-03 00:06:14 +0200

    add: mapper, serializer, saver

--- lab_6/keyboard_class.py
+++ lab_6/keyboard_class.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import List, Dict
+from typing import List, Dict, Any
+import json
+import os
 
 
 class ICommand(ABC):
@@ -11,21 +13,61 @@ class ICommand(ABC):
     def undo(self) -> None:
         pass
 
+    @abstractmethod
+    def get_params(self) -> Dict:
+        pass
+
 
 class Keyboard:
     def __init__(self):
         self.keys: Dict[str, ICommand] = {}
-        self._history: List[str] = []
-        self._redo_stack: List[str] = []
         self.text_buffer: List[str] = []
+        self._history: List[ICommand] = []
+        self._redo_stack: List[ICommand] = []
 
-    def bind(self, hot_keys: str, command: ICommand) -> None:
-        self.keys[hot_keys] = command
+    def bind(self, key_combo: str, command: ICommand) -> None:
+        self.keys[key_combo] = command
 
-    def press(self, hot_keys: str):
-        if hot_keys in self.keys:
-            ...
+    def press(self, key_combo: str) -> None:
+        if key_combo in self.keys:
+            command = self.keys[key_combo]
+            command.execute()
 
+            self._history.append(command)
+            self._redo_stack.clear()
+        else:
+            print('Command is not define')
+
+    def undo(self) -> None:
+        if self._history:
+            command = self._history.pop()
+            command.undo()
+            self._redo_stack.append(command)
+
+    def redo(self) -> None:
+        if self._redo_stack:
+            command = self._redo_stack.pop()
+            command.execute()
+
+            self._history.append(command)
+
+
+class KeyCommand(ICommand):
+    def __init__(self, buffer: List, char: str):
+        self.buffer = buffer
+        self.char = char
+
+    def execute(self) -> None:
+        self.buffer.append(self.char)
+        print(''.join(self.buffer))
+
+    def undo(self) -> None:
+        if self.buffer:
+            self.buffer.pop()
+            print(''.join(self.buffer))
+
+    def get_params(self) -> Dict:
+        return {'char': self.char}
 
 
 class VolumeUpCommand(ICommand):
@@ -35,6 +77,9 @@ class VolumeUpCommand(ICommand):
     def undo(self) -> None:
         print('Volume decreased -10%')
 
+    def get_params(self) -> Dict:
+        return {}
+
 
 class VolumeDownCommand(ICommand):
         def execute(self) -> None:
@@ -42,6 +87,9 @@ class VolumeDownCommand(ICommand):
 
         def undo(self) -> None:
             print('Volume increased +10%')
+
+        def get_params(self) -> Dict:
+            return {}
 
 
 class MediaPlayerCommand(ICommand):
@@ -51,6 +99,72 @@ class MediaPlayerCommand(ICommand):
     def undo(self) -> None:
         print('Media player closed')
 
+    def get_params(self) -> Dict:
+        return {}
 
-class KeybordStateSaver(ICommand)   :
-    ...
+
+CLASS_REGISTRY = {
+    'KeyCommand': KeyCommand,
+    'VolumeUpCommand': VolumeUpCommand,
+    'VolumeDownCommand': VolumeDownCommand,
+    'MediaPlayerCommand': MediaPlayerCommand
+}
+
+
+class Mapper:
+    @staticmethod
+    def map_to_dict(keys_dict: Dict[str, ICommand]) -> Dict:
+        data = {}
+        for key_combo, command in keys_dict.items():
+            cmd_class = type(command).__name__
+            data[key_combo] = {
+                'cmd_class': cmd_class,
+                'args': command.get_params()
+            }
+        return data
+
+    @staticmethod
+    def map_from_dict(data: Dict[str, Any], buffer: List[str]) -> Dict:
+        restored_keys = {}
+        for key_combo, info in data.items():
+            cls_name = info.get('cmd_class', {})
+            args = info.get('args', {})
+            cmd_class = CLASS_REGISTRY.get(cls_name)
+            if cmd_class:
+                if cls_name == 'KeyCommand':
+                    command = cmd_class(buffer, **args)
+                else:
+                    command = cmd_class(**args)
+                restored_keys[key_combo] = command
+
+        return restored_keys
+
+
+class Serializer:
+    @staticmethod
+    def serialize(filename: str, data: Dict):
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4)
+
+    @staticmethod
+    def deserialize(filename: str) -> Dict:
+        with open(filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+
+class KeyboardStateSaver:
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def save_state(self, keyboard: Keyboard):
+        data_dict = Mapper.map_to_dict(keyboard.keys)
+        Serializer.serialize(self.filename, data_dict)
+
+    def restore_state(self, keyboard: Keyboard) -> bool:
+        if not os.path.exists(self.filename):
+            return False
+        data_dict = Serializer.deserialize(self.filename)
+
+        new_keys = Mapper.map_from_dict(data_dict, keyboard.text_buffer)
+        keyboard.keys = new_keys
+        return True
